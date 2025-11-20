f667961a541074f026056991fcf04e7caee31368
daanberger <daan.berger@telenet.be>
2025-11-20 13:58:02 +0100

    Add Post service interface, implementation, and controller for retrieving posts

---
+++ backend-java/NewsApp/post-service/src/main/java/be/pxl/services/controller/PostController.java
@@ -0,0 +1,25 @@
+package be.pxl.services.controller;
+
+import be.pxl.services.domain.Post;
+import be.pxl.services.services.IPostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final IPostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getPosts() {
+        return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.OK);
+    }
+}
---
+++ backend-java/NewsApp/post-service/src/main/java/be/pxl/services/services/PostService.java
@@ -0,0 +1,20 @@
+package be.pxl.services.services;
+
+import be.pxl.services.domain.Post;
+import be.pxl.services.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService implements IPostService{
+
+    private final PostRepository repository;
+
+    @Override
+    public List<Post> getAllPosts() {
+        return repository.findAll();
+    }
+}
