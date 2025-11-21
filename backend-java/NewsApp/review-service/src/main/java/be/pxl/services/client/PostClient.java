643f576dd1419624b4677e96d186c4ccb06fa0db
daanberger <daan.berger@telenet.be>
2025-11-21 18:40:28 +0100

    Add application properties and Docker Compose for MySQL setup; update PostClient with endpoint mappings and fix parent version

--- backend-java/NewsApp/review-service/src/main/java/be/pxl/services/client/PostClient.java
+++ backend-java/NewsApp/review-service/src/main/java/be/pxl/services/client/PostClient.java
@@ -3,11 +3,15 @@ package be.pxl.services.client;
 import be.pxl.services.domain.dto.PostResponse;
 import be.pxl.services.domain.dto.PostStatusRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "post-service", url = "http://localhost:8081")
+@FeignClient(name = "post-service", url = "http://localhost:8081") // mock url for testing
 public interface PostClient {
-    List<PostResponse> getPendingPosts(String role);
-    void updatePostStatus(Long post, PostStatusRequest statusRequest);
+    @GetMapping("/api/post/editor?status=PENDING")
+    List<PostResponse> getPendingPosts(@RequestHeader("X-Role") String role);
+
+    @PutMapping("/api/post/{postId}/status")
+    void updatePostStatus(@PathVariable Long postId, @RequestBody PostStatusRequest statusRequest);
 }
